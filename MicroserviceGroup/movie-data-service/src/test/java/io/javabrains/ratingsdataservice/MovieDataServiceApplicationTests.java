cda912148f4b27d3e24fda817462a38c1e445679
Nilesh Kumar <nileshkumar6@deloitte.com>
2022-03-01 13:48:24 +0530

    Microservice Self Practise

---
+++ MicroserviceGroup/movie-data-service/src/test/java/io/javabrains/ratingsdataservice/MovieDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
